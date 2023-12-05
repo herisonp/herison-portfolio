2edf2a4bdff9910c4c301a207208107f9220925a
Herison Pereira <herisonup@gmail.com>
2023-12-05 02:10:24 -0300

    feat: create section component wordpress

---
+++ src/components/wordpress/wordpress-item.tsx
@@ -0,0 +1,46 @@
+"use client";
+import * as HoverCard from "@radix-ui/react-hover-card";
+import { AspectRatio } from "@radix-ui/themes";
+import Image from "next/image";
+
+export interface WordpressItemProps {
+	title: string;
+	link: string;
+	image: string;
+}
+
+export default function WordpressItem({
+	project
+}: {
+	project: WordpressItemProps;
+}) {
+	return (
+		<li>
+			<HoverCard.Root>
+				<HoverCard.Trigger asChild>
+					<a href={project.link} target="_blank" className="underline">
+						{project.title}
+					</a>
+				</HoverCard.Trigger>
+				<HoverCard.Portal>
+					<HoverCard.Content
+						className="w-[300px] rounded-md bg-neutral-800/50 p-1 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] data-[state=open]:transition-all overflow-hidden"
+						sideOffset={5}
+					>
+						<AspectRatio ratio={16 / 9}>
+							<Image
+								src={project.image}
+								alt={project.title}
+								width={300}
+								height={300}
+								className="w-full object-cover"
+							/>
+						</AspectRatio>
+
+						<HoverCard.Arrow className="fill-neutral-800/50" />
+					</HoverCard.Content>
+				</HoverCard.Portal>
+			</HoverCard.Root>
+		</li>
+	);
+}
---
+++ src/components/wordpress/wordpress.tsx
@@ -0,0 +1,57 @@
+import { InfoCircledIcon } from "@radix-ui/react-icons";
+import { Container, Section, Heading, Box, Flex, Text } from "@radix-ui/themes";
+import WordpressItem, { WordpressItemProps } from "./wordpress-item";
+import { GetProjects } from "@/functions/get-projects";
+
+export async function Wordpress() {
+	const projects: WordpressItemProps[] = await GetProjects("/api/wordpress");
+	return (
+		<Section py={"3"}>
+			<Container size={"3"} px={"3"}>
+				<Heading
+					as="h2"
+					className="text-neutral-300"
+					size={"7"}
+					weight={"bold"}
+				>
+					Sites com Elementor + Wordpress
+				</Heading>
+				<Box
+					p={"4"}
+					mt={"4"}
+					width={"100%"}
+					className="bg-neutral-800/50 rounded-md"
+				>
+					<Flex align={"center"} gap={"2"}>
+						<InfoCircledIcon className="shrink-0" width={16} height={16} />
+						<Text>
+							Sites desenhados e desenvolvidos com Elementor + Wordpress por mim
+						</Text>
+					</Flex>
+				</Box>
+				<Box
+					p={"4"}
+					mt={"4"}
+					width={"100%"}
+					className="bg-yellow-400/20 text-amber-300/80 rounded-md"
+				>
+					<Flex align={"center"} gap={"2"}>
+						<InfoCircledIcon className="shrink-0" width={16} height={16} />
+						<Text>
+							São links reais em produção, onde a maioria não são mantidos por
+							mim. Desde já, peço desculpas caso algum não esteja mais
+							disponível.
+						</Text>
+					</Flex>
+				</Box>
+				<Flex asChild direction={"column"} mt={"6"} gap={"2"}>
+					<ol className="list-decimal pl-5">
+						{projects.map((project) => (
+							<WordpressItem key={project.link} project={project} />
+						))}
+					</ol>
+				</Flex>
+			</Container>
+		</Section>
+	);
+}
