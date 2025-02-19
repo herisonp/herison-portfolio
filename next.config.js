35db629f587b85cc869fb9b0935d18669b99619a
Herison Pereira <herisonup@gmail.com>
2025-02-19 13:45:43 -0300

    refactor: remove unused PageTeste component and clean up next.config.js

--- next.config.js
+++ next.config.js
@@ -8,12 +8,6 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
-      {
-        protocol: "https",
-        hostname: "lp.idiomus.com",
-        port: "",
-        pathname: "/**",
-      },
     ],
   },
 };
