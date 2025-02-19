7a011a85f01f7b98967498abe7aad317a6de0ff8
Herison Pereira <herisonup@gmail.com>
2025-02-19 11:52:39 -0300

    feat: add new image source and create PageTeste component

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "herisonpereira.com.br",
-				port: "",
-				pathname: "/**"
-			}
-		]
-	}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "herisonpereira.com.br",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lp.idiomus.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
---
+++ src/app/teste/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+
+export default function PageTeste() {
+  return (
+    <div>
+      PageTeste
+      <Image
+        src="https://lp.idiomus.com/wp-content/uploads/2025/01/freepik__imagine-a-powerful-tempest-raging-within-the-ruins__77413.jpeg"
+        alt="image teste"
+        width={800}
+        height={800}
+      />
+    </div>
+  );
+}
